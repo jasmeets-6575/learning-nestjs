14be6ae047d8616818ed2309b5864a3832234b03
Jasmeet Singh <jasmeetsonu990@gmail.com>
2026-06-13 15:27:35 +0530

    learning authorization and pipes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productsService: ProductService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   getProducts() {
     return this.productsService.getAllProducts();
   }
