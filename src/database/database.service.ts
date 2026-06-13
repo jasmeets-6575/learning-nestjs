711b9b778d0bb12732da3d37de4f0201e0257859
Jasmeet Singh <jasmeetsonu990@gmail.com>
2026-06-13 17:07:55 +0530

    learning middleware , envs, db setup

---
+++ src/database/database.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+@Injectable()
+export class DatabaseService {
+  private isConnected = false;
+
+  onModuleInit() {
+    this.isConnected = true;
+    console.log('Database Connected');
+  }
+  onApplicationShutdown(signal: string) {
+    this.isConnected = false;
+    console.log(`Database disconnected due to app shutdown. Signal ${signal}`);
+  }
+
+  getStatus() {
+    return this.isConnected ? 'Connected' : 'Disconnected';
+  }
+}
